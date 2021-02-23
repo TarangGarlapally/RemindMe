96f6d3209a31284f967766fdba78a8bf0833d3cf
TarangGarlapally <taranggarlapally@gmail.com>
2021-02-23 20:40:35 +0530

    added sound

--- helper.js
+++ helper.js
@@ -4,7 +4,9 @@ const checkReminder = ()=>{
 	const msg = localStorage.getItem(timestamp);
 	//console.log(Math.floor(Date.now()/1000));
 	if(msg !== null){
-		alert(msg);
+		var audio = new Audio('1_second_tone.mp3');
+		audio.play();
+		setTimeout(()=>{alert(msg)},200);
 		localStorage.removeItem(timestamp);
 	}
 }
