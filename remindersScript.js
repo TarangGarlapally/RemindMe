092a237052405994e453097b62ee8c636ac06e07
TarangGarlapally <taranggarlapally@gmail.com>
2022-01-22 23:32:47 +0530

    updated reminderTiles

--- remindersScript.js
+++ remindersScript.js
@@ -1,5 +1,25 @@
 alert("hi");
 
+function parseTime(hours, minutes) { 
+    var meridian = "";
+    if(hours<12)
+        meridian = "AM";
+    else
+        meridian = "PM";
+
+    if(minutes<10)
+        minutes = "0" + minutes;
+
+    if(hours > 12)
+        hours = hours - 12;
+    if(hours<10){
+        hours = "0" + hours;
+    }
+    if(hours === 0)
+        return "12:" + minutes + " " + meridian;
+    return hours + ":" + minutes + " " + meridian;
+}
+
 
 
 var listOfTimestamps = [];
@@ -13,8 +33,8 @@ listOfTimestamps.sort();
 for(i = 0; i<listOfTimestamps.length; i++){
     x = listOfTimestamps[i];
     const date = new Date(parseInt(x)*1000);
-    const dateTime = date.getDate()+'/'+(date.getMonth()+1)+'/'+date.getFullYear();
-    document.getElementById("allRemindersBox").innerHTML += "<div id = '"+x+"' style = 'padding:10px; background-color:lightblue; margin:4px;'>"+x+" : "+dateTime+" : "+localStorage[x]+"</div>";
+    const dateTime = date.getDate()+'/'+(date.getMonth()+1)+'/'+date.getFullYear() + " &nbsp;&nbsp;" + parseTime(date.getHours(), date.getMinutes());
+    document.getElementById("allRemindersBox").innerHTML += "<div id = '"+x+"' class = 'reminderTile'><p class = 'reminderMessage'><span class='glyphicon glyphicon-calendar'></span> &nbsp;"+dateTime+"&nbsp;&nbsp;&nbsp;&nbsp;"+localStorage[x]+"</p><p class = 'reminderOptions'></p></div>";
     
     
     console.log(document.getElementById(x))
